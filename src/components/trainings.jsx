1053c2806a8c42ecced4962645fb39f7d78a1820
levin <levinthiel@proton.me>
2025-10-05 07:42:33 +0200

    adjust responsiveness in all components

--- src/components/trainings.jsx
+++ src/components/trainings.jsx
@@ -6,20 +6,24 @@ export default function Trainings({ id, t, setLang, lang }) {
             <h2>{t("trainingH2")}</h2>
             <CourseContainer>
                 <div>
-                    <h3>Web Developer </h3>
+                    <h3>{t("training1H3")}</h3>
                     <p>@Neue Fische (2024)</p>
                 </div>
                 <div>
-                    <h3>Web Developer </h3>
-                    <p>@Neue Fische (2024)</p>
+                    <h3>{t("training2H3")}</h3>
+                    <p>@Cimadata Akademie (2021)</p>
                 </div>
                 <div>
-                    <h3>Web Developer </h3>
-                    <p>@Neue Fische (2024)</p>
+                    <h3>{t("training3H3")}</h3>
+                    <p>@AFP Köln(2014)</p>
                 </div>
                 <div>
-                    <h3>Web Developer </h3>
-                    <p>@Neue Fische (2024)</p>
+                    <h3>{t("training4H3")}</h3>
+                    <p>@Ecole Pivaut (Nantes - FR) (2008-2012)</p>
+                </div>
+                <div>
+                    <h3>{t("training5H3")}</h3>
+                    <p>@Lycée J.Loth (Pontivy - FR) (2005-2008)</p>
                 </div>
             </CourseContainer>
         </TrainingsContainer>
